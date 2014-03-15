cbb521b8c9dd36f25df8d6dcc81905673997b848
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-15 13:28:59 -0600

    Add additional attributes to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315175117) do
+ActiveRecord::Schema.define(version: 20140315185404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20140315175117) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "email"
+    t.text     "headline"
+    t.text     "summary"
+    t.string   "industry"
+    t.string   "location"
+    t.string   "image"
+    t.string   "public_profile"
   end
 
 end

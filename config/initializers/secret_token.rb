01a36025381b6dbf1b2d9de4ca15b0e12a101e6a
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-15 11:22:49 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwiftIntro::Application.config.secret_key_base = 'a6af6fa3314a42bcc0b43a86be54e1f38bd929b9a5340103c0640d76c3a3cfe0b55636c35155a3b317750de9d2168b82f877ce55c60b54ae1933b463a8646557'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwiftIntro::Application.config.session_store :cookie_store, key: '_swift_intro_session'

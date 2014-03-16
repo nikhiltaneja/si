9f516d8a570db9afb169f84de765daf3b6987b65
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-16 15:35:03 -0600

    Add job model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_one :location
   has_many :educations
+  has_many :jobs
 
   def self.from_omniauth(auth)
     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
@@ -23,9 +24,19 @@ class User < ActiveRecord::Base
     end
 
     user_educations = auth["extra"]["raw_info"]["educations"]["values"]
+    current_jobs = auth["extra"]["raw_info"]["threeCurrentPositions"]["values"]
+    past_jobs = auth["extra"]["raw_info"]["threePastPositions"]["values"]
 
     if user_educations
       add_educations(user_educations, user.id)
+    end
+
+    if current_jobs
+      add_current_jobs(current_jobs, user.id)
+    end
+
+    if past_jobs
+      add_past_jobs(past_jobs, user.id)
     end
 
     user
@@ -37,6 +48,22 @@ class User < ActiveRecord::Base
       subject = Subject.find_or_create_by(name: education.fieldOfStudy)
       degree = Degree.find_or_create_by(name: education.degree)
       Education.create!(user_id: user_id, school_id: school.id, subject_id: subject.id, degree_id: degree.id)
+    end
+  end
+
+  def self.add_current_jobs(current_jobs, user_id)
+    current_jobs.each do |job|
+      company = Company.find_or_create_by(name: job["company"].name)
+      position = Position.find_or_create_by(name: job["title"])
+      Job.create(user_id: user_id, company_id: company.id, position_id: position.id)
+    end
+  end
+
+  def self.add_past_jobs(past_jobs, user_id)
+    past_jobs.each do |job|
+      company = Company.find_or_create_by(name: job["company"].name)
+      position = Position.find_or_create_by(name: job["title"])
+      Job.create(user_id: user_id, company_id: company.id, position_id: position.id)
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140316203034) do
+ActiveRecord::Schema.define(version: 20140316212833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "degrees", force: true do |t|
     t.datetime "created_at"
@@ -36,6 +42,18 @@ ActiveRecord::Schema.define(version: 20140316203034) do
   add_index "educations", ["subject_id"], name: "index_educations_on_subject_id", using: :btree
   add_index "educations", ["user_id"], name: "index_educations_on_user_id", using: :btree
 
+  create_table "jobs", force: true do |t|
+    t.integer  "company_id"
+    t.integer  "position_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "jobs", ["company_id"], name: "index_jobs_on_company_id", using: :btree
+  add_index "jobs", ["position_id"], name: "index_jobs_on_position_id", using: :btree
+  add_index "jobs", ["user_id"], name: "index_jobs_on_user_id", using: :btree
+
   create_table "locations", force: true do |t|
     t.string   "area"
     t.integer  "user_id"
@@ -44,6 +62,12 @@ ActiveRecord::Schema.define(version: 20140316203034) do
   end
 
   add_index "locations", ["user_id"], name: "index_locations_on_user_id", using: :btree
+
+  create_table "positions", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "schools", force: true do |t|
     t.string   "name"

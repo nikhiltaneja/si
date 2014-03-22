a44bb9910c46a3b83364d233da0d286ce7dac8cc
JSchulhof <JSchulhof@gmail.com>
2014-03-22 10:28:42 -0700

    working update

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,7 @@ class User < ActiveRecord::Base
   has_many :second_users, class_name: "Match", foreign_key: :second_user_id
 
   def self.from_omniauth(auth)
-    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
-  end
-
-  def self.create_from_omniauth(auth)
-    user = create! do |user|
+    where(auth.slice("provider", "uid")).first_or_initialize.tap do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.first_name = auth["info"]["first_name"]
@@ -25,31 +21,32 @@ class User < ActiveRecord::Base
       user.location = Location.find_or_create_by(area: auth["info"]["location"])
       user.image = auth["info"]["image"]
       user.public_profile = auth["info"]["urls"]["public_profile"]
+      user.save!
+    
+      user_educations = auth["extra"]["raw_info"]["educations"]["values"]
+      current_jobs = auth["extra"]["raw_info"]["threeCurrentPositions"]["values"]
+      past_jobs = auth["extra"]["raw_info"]["threePastPositions"]["values"]
+
+
+      user_connections = auth["extra"]["raw_info"]["connections"]["values"]
+
+      if user_educations
+        add_educations(user_educations, user.id)
+      end
+
+      if current_jobs
+        add_current_jobs(current_jobs, user.id)
+      end
+
+      if past_jobs
+        add_past_jobs(past_jobs, user.id)
+      end
+
+      if user_connections
+        add_connections(user_connections, user.id)
+      end
+      user.save!
     end
-
-    user_educations = auth["extra"]["raw_info"]["educations"]["values"]
-    current_jobs = auth["extra"]["raw_info"]["threeCurrentPositions"]["values"]
-    past_jobs = auth["extra"]["raw_info"]["threePastPositions"]["values"]
-
-    user_connections = auth["extra"]["raw_info"]["connections"]["values"]
-
-    if user_educations
-      add_educations(user_educations, user.id)
-    end
-
-    if current_jobs
-      add_current_jobs(current_jobs, user.id)
-    end
-
-    if past_jobs
-      add_past_jobs(past_jobs, user.id)
-    end
-
-    if user_connections
-      add_connections(user_connections, user.id)
-    end
-
-    user
   end
 
   def self.add_educations(user_educations, user_id)
@@ -57,7 +54,7 @@ class User < ActiveRecord::Base
       school = School.find_or_create_by(name: education.schoolName)
       subject = Subject.find_or_create_by(name: education.fieldOfStudy)
       degree = Degree.find_or_create_by(name: education.degree)
-      Education.create!(user_id: user_id, school_id: school.id, subject_id: subject.id, degree_id: degree.id)
+      Education.find_or_create_by(user_id: user_id, school_id: school.id, subject_id: subject.id, degree_id: degree.id)
     end
   end
 
@@ -65,7 +62,7 @@ class User < ActiveRecord::Base
     current_jobs.each do |job|
       company = Company.find_or_create_by(name: job["company"].name)
       position = Position.find_or_create_by(name: job["title"])
-      Job.create(user_id: user_id, company_id: company.id, position_id: position.id)
+      Job.find_or_create_by(user_id: user_id, company_id: company.id, position_id: position.id)
     end
   end
 
@@ -73,14 +70,14 @@ class User < ActiveRecord::Base
     past_jobs.each do |job|
       company = Company.find_or_create_by(name: job["company"].name)
       position = Position.find_or_create_by(name: job["title"])
-      Job.create(user_id: user_id, company_id: company.id, position_id: position.id)
+      Job.find_or_create_by(user_id: user_id, company_id: company.id, position_id: position.id)
     end
   end
 
   def self.add_connections(user_connections, user_id)
     user_connections.each do |connection|
       linkedin_user = LinkedinUser.find_or_create_by(uid: connection["id"])
-      Connection.create(user_id: user_id, linkedin_user_id: linkedin_user.id)
+      Connection.find_or_create_by(user_id: user_id, linkedin_user_id: linkedin_user.id)
     end
   end
  

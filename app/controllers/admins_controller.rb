cd184c17a91d71f97b7d56b25bc44acc4e1b8a65
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-23 13:05:11 -0700

    Add methods to remove users that one should not match with

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,23 +2,29 @@ class AdminsController < ApplicationController
   before_action :admin?
 
   def index
-    @eligible_users = []
-    @users = User.all
-    @users.each do |user|
-      if (Time.now - user.current_match.created_at) > 10 #need to change to one day
-        @eligible_users << user
+    users = User.all
+    @eligible_users  = users.select do |user|
+      if user.current_match
+        (Time.now - user.current_match.created_at) > 10 #need to change to one day
       end
     end
   end
 
   def show
     @user = User.find(params[:id])
-    @location_matches = @user.location_matches
-    @shared = []
-    @location_matches.each do |user_l|
-      @shared << @user.shared_connections(user_l).count
-    end
-    
-  end
 
+    users_same_location = @user.location_matches
+    removed_previous_matches = @user.remove_previous_matches(users_same_location)
+    @potential_matches = @user.remove_oneself(removed_previous_matches)
+
+    @shared = []
+    @potential_matches.each do |potential_match|
+      @shared << @user.shared_connections(potential_match).count
+    end
+
+    # users_same_location = @user.location_matches
+    # @shared_counts = users_same_location.collect do |user_same_location|
+    #   @user.shared_connections(user_same_location).count
+    # end
+  end
 end

a5872c2618104a8e612e92da0e840042122f3b06
JSchulhof <JSchulhof@gmail.com>
2014-03-24 20:08:40 -0700

    connection worked fixed

--- app/workers/connection_worker.rb
+++ app/workers/connection_worker.rb
@@ -2,6 +2,6 @@ class ConnectionWorker
   include Sidekiq::Worker
 
   def perform(connections, user_id)
-    add_connections(connections, user_id)
+    User.add_connections(connections, user_id)
   end
 end

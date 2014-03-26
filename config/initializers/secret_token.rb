146aefbcfc23be9a6e5a06891fed18acc53333dc
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-26 17:43:34 -0600

    Update environment variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SwiftIntro::Application.config.secret_key_base = 'a6af6fa3314a42bcc0b43a86be54e1f38bd929b9a5340103c0640d76c3a3cfe0b55636c35155a3b317750de9d2168b82f877ce55c60b54ae1933b463a8646557'
+SwiftIntro::Application.config.secret_key_base = ENV['SECRET_KEY']

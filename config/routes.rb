cabbced931797da9f3c0cd9448fb144256aece74
JSchulhof <JSchulhof@gmail.com>
2014-04-03 15:05:07 -0700

    shows user path when logging in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'sidekiq/web'
 SwiftIntro::Application.routes.draw do
   
   get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
+  get 'auth/failure', to: redirect("/")
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
   root to: 'users#show'

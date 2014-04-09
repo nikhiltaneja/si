c673ba38c753870ffec7af622c32ddd9c7c62d20
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-04-08 22:31:13 -0600

    Update initial form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Industry.find_or_create_by(name: "Education")
-Industry.find_or_create_by(name: "Fashion")
+Industry.find_or_create_by(name: "Energy")
 Industry.find_or_create_by(name: "Finance")
 Industry.find_or_create_by(name: "Food")
 Industry.find_or_create_by(name: "Government")

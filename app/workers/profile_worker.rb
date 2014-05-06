672a8f9a641610781b656384c9e17370078c5309
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-05-05 18:29:49 -0600

    Update signup form

--- app/workers/profile_worker.rb
+++ app/workers/profile_worker.rb
@@ -4,11 +4,11 @@ class ProfileWorker
   def perform(user_id)
     user = User.find(user_id)
 
-    if user.topic_interests.empty?
-      TopicInterest.create!(user_id: user.id, topic_id: 1)
-      TopicInterest.create!(user_id: user.id, topic_id: 2)
-      TopicInterest.create!(user_id: user.id, topic_id: 3)
-    end
+    # if user.topic_interests.empty?
+    #   TopicInterest.create!(user_id: user.id, topic_id: 1)
+    #   TopicInterest.create!(user_id: user.id, topic_id: 2)
+    #   TopicInterest.create!(user_id: user.id, topic_id: 3)
+    # end
     
     User.get_educations(user)
     User.get_jobs(user)

8c2c9bde1a9c7ea9d38ad9d357b20df32eb16a49
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-05-14 15:31:56 -0600

    Move flash notices below navbar, move algorithm logic to module

---
+++ app/models/algorithm.rb
@@ -0,0 +1,13 @@
+module Algorithm
+  def rate_shared_skills(shared_skills_count)
+    shared_skills_count * 5
+  end
+
+  def rate_shared_industry_interests(shared_industry_interests_count)
+    shared_industry_interests_count * 3
+  end
+
+  def rate_shared_connections(shared_connections)
+    shared_connections * 2
+  end
+end

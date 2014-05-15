7ba5422150b02deed864337872b2b4783cb8e945
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-05-14 18:49:45 -0600

    Refine algorithm

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -1,13 +1,31 @@
 module Algorithm
   def rate_shared_skills(shared_skills_count)
-    shared_skills_count * 5
+    if shared_skills_count == 0
+      3
+    elsif shared_skills_count > 15
+      60
+    else  
+      shared_skills_count * 4
+    end
   end
 
   def rate_shared_industry_interests(shared_industry_interests_count)
-    shared_industry_interests_count * 3
+    if shared_industry_interests_count == 0
+      2
+    elsif shared_industry_interests_count > 4
+      20
+    else
+      shared_industry_interests_count * 5
+    end
   end
 
   def rate_shared_connections(shared_connections)
-    shared_connections * 2
+    if shared_connections > 1 && shared_connections <= 5
+      shared_connections * 3
+    elsif shared_connections > 60
+      shared_connections
+    else
+      shared_connections * 2
+    end
   end
 end

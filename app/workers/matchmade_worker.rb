3bde26356c027921565bb646dc11befbcb572589
JSchulhof <JSchulhof@gmail.com>
2014-05-15 10:43:14 -0700

    check if user deleted

--- app/workers/matchmade_worker.rb
+++ app/workers/matchmade_worker.rb
@@ -4,6 +4,8 @@ class MatchmadeWorker
   def perform(user1_id, user2_id)
     user1 = User.find(user1_id)
     user2 = User.find(user2_id)
-    UserMailer.match_confirmation(user1, user2).deliver
+    if user1.deleted == false && user2.deleted == false
+      UserMailer.match_confirmation(user1, user2).deliver
+    end
   end
 end
